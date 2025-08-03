e9d6f49c0a1752a79a3212a0a06ac765bd78c0d3
victor <victor.rascle@lyceeastier.com>
2025-08-03 09:48:09 +0200

    add ci

--- config/config_test.go
+++ config/config_test.go
@@ -2,19 +2,19 @@ package config
 
 import (
 	"flag"
+	"os"
 	"testing"
 )
 
 func TestParseArgsDefault(t *testing.T) {
-
-	args := []string{"cmd"}
-	oldArgs := flag.CommandLine
-	flag.CommandLine = flag.NewFlagSet(args[0], flag.ExitOnError)
-	defer func() { flag.CommandLine = oldArgs }()
+	oldArgs := os.Args
+	defer func() { os.Args = oldArgs }()
+	os.Args = []string{"cmd"}
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 
 	config := ParseArgs()
 
 	if config.Method != "GET" {
-		t.Errorf("Méthode par défaut incorrecte: attendu GET, obtenu %s", config.Method)
+		t.Errorf("Méthode par défaut incorrecte : attendu GET, obtenu %s", config.Method)
 	}
 }
