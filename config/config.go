bedc9b3d399d5fea90f7caacf083485d16f4fec8
victor <victor.rascle@lyceeastier.com>
2025-08-03 09:39:59 +0200

    ajout de help

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"strings"
 )
 
@@ -21,6 +22,12 @@ type Config struct {
 }
 
 func ParseArgs() Config {
+	flag.Usage = func() {
+		fmt.Println("Usage : fuzzer [options]")
+		fmt.Println("Options disponibles :")
+		flag.PrintDefaults()
+	}
+
 	url := flag.String("url", "", "URL de l'endpoint API à tester")
 	method := flag.String("method", "GET", "Méthode HTTP (GET, POST, PUT, etc.)")
 	category := flag.String("category", "XSS", "Catégorie de payloads")
@@ -30,12 +37,11 @@ func ParseArgs() Config {
 	wordlist := flag.String("wordlist", "", "Chemin vers une wordlist personnalisée")
 	threads := flag.Int("threads", 10, "Nombre de threads (goroutines) pour le fuzzing concurrent")
 	urlList := flag.String("urllist", "", "Chemin vers un fichier contenant une liste d’URLs à fuzz (1 par ligne)")
-	var auth = flag.String("auth", "", "Header Authorization (ex: Bearer <token>)")
-	var cookies = flag.String("cookies", "", "Cookies HTTP à inclure (ex: sessionid=abc123; token=xyz)")
+	auth := flag.String("auth", "", "Header Authorization (ex: Bearer <token>)")
+	cookies := flag.String("cookies", "", "Cookies HTTP à inclure (ex: sessionid=abc123; token=xyz)")
+	contentType := flag.String("contenttype", "application/json", "Type de contenu à envoyer")
 
 	flag.Parse()
-
-	contentType := flag.String("contenttype", "application/json", "Type de contenu à envoyer (application/json, multipart/form-data, application/x-www-form-urlencoded)")
 
 	return Config{
 		URL:         *url,
